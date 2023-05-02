21e5ee5f586ff1459ccee7e24d04ac892f10b9f3
thegu5 <58223632+thegu5@users.noreply.github.com>
2023-05-02 17:44:40 -0400

    cleanup + no favicon :(

--- index.js
+++ index.js
@@ -1,70 +1,56 @@
 const async = require('async');
 const fs = require('fs');
 const util = require('minecraft-server-util');
-// ips.txt contains a list of IP addresses, one per line
+const action = process.env.GITHUB_ACTIONS;
 let ips = fs.readFileSync('ips.txt').toString().split('\n');
-const startTime = new Date();
 let results = [];
 
 // async every 10 seconds, save results to data.json
 const saveData = async () => {
     fs.writeFileSync('data.json', JSON.stringify(results));
 }
-setInterval(saveData, 5000);
+if (!action) setInterval(saveData, 5000);
+
 const progress = () => {
     // calculate percentage complete
     let percent = (found + dead) / ips.length * 100;
     percent = percent.toFixed(2);
-    // calculate time passed
-    let timePassed = new Date() - startTime;
-    // format time passed
     process.stdout.write("%" + percent + " found " + found + " dead " + dead + " remaining " + remaining + "\r");
 }
-setInterval(progress, 5000); // slow but github actions cries if it's too fast
+action ? setInterval(progress, 5000) : setInterval(progress, 500);
+
 let found = 0;
 let dead = 0;
 let remaining = ips.length;
+
 const getServerStatus = async (ip) => {
     remaining--;
     ip = ip.trim();
     return util.status(ip)
     .then((response) => {
+        delete response.favicon;
         results.push(response);
-        // console.log("len " + results.length)
-        // console.log(ip + ": " + response)
         found++;
-        // progress();
         return true;
     })
     .catch((error) => {
-        // console.log(ip + ": " + error)
         dead++;
-        // progress();
         return false;
-        
     });
 }
 
-// let q = async.queue(getServerStatus, 1000);
-
-// ips.forEach((ip) => {
-//     // console.log("pushed " + ips.indexOf(ip))
-//     q.push(ip);
-// });
-// ips = null;
-// q.drain = () => {
-//     fs.writeFileSync('data.json', JSON.stringify(results));
-//     console.log("done");
-// }
 async.eachLimit(ips, 500, getServerStatus)
-.then((res) => {
-    fs.writeFileSync('data.json', JSON.stringify(results));
-    progress();
-    console.log();
-    console.log("done");
-    process.exit(0);
-})
-.catch((err) => {
-    console.log(err)
-    process.exit(1);
-})
+    .then((res) => {
+        // const zlib = require('zlib');
+        // const gzipData = zlib.gzipSync(JSON.stringify(results));
+        // fs.writeFileSync('data.json.gz', gzipData);
+        fs.writeFileSync('data.json', JSON.stringify(results));
+        progress();
+        console.log();
+        console.log("done");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.log(err)
+        process.exit(1);
+    })
